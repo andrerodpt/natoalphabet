6308593c3cb2bc9202ed939ed07809a53ff37a7d
Andre Rodrigues <andre.rodrigues@giffits.de>
2023-09-12 14:01:39 +0100

    feature: added error handling

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ data = pd.read_csv('nato_phonetic_alphabet.csv')
 
 nato_phonetic_alphabet_list = {row.letter:row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter a word: ").upper()
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
 
-phonetic_translation = [nato_phonetic_alphabet_list[letter] for letter in user_input]
+    try:
+        phonetic_translation = [nato_phonetic_alphabet_list[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_translation)
 
-print(phonetic_translation)
 
-
+generate_phonetic()
